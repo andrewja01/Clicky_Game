984dce37a34b9e866317d7f028a2dbd57604ccde
AndyJ <andrewja01@gmail.com>
2019-10-04 13:26:37 -0400

    changing logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const Header = props => (
-  <div className='hero text-center col-xl'>
-    <h2>Clicky Game!</h2>
+  <div className='hero text-center col-xl row align-items-center justify-content-center'>
     <h4>
       click an image to increase your score. If you click the same image twice
       its GAME OVER
